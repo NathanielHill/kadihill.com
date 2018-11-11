b2251959180a6f2ec03929d69aeae8a37d54f1ff
Nathaniel Hill <nata@goguna.com>
2018-11-11 12:47:14 -0500

    styles shouldn't go in _document.js

--- www/components/Layout.js
+++ www/components/Layout.js
@@ -12,6 +12,24 @@ export const Layout = ({ children, title = 'Kadi Hill' }) => (
       {children}
     </main>
     <Footer />
+    <style jsx global>{`
+      * {
+        margin: 0;
+        padding: 0;
+      }
+      html, body {
+        width: 100vw;
+        min-height: 100vh;
+        overscroll-behavior: contain;
+        font-size: 2vmin;
+        font-family: Verdana, Geneva, sans-serif;
+        overflow-x: hidden;
+      }
+      body {
+        background-color: rgb(168, 245, 206);
+        background-image: linear-gradient(-15deg, rgb(248, 198, 178), rgb(253, 237, 231));
+      }
+    `}</style>
     <style jsx>{`
       div {
         display: flex;
--- www/pages/_document.js
+++ www/pages/_document.js
@@ -12,24 +12,6 @@ export default class MyDocument extends Document {
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta name='description' content='Kadi Hill' />
-        <style jsx global>{`
-          * {
-            margin: 0;
-            padding: 0;
-          }
-          html, body {
-            width: 100vw;
-            min-height: 100vh;
-            overscroll-behavior: contain;
-            font-size: 2vmin;
-            font-family: Verdana, Geneva, sans-serif;
-            overflow-x: hidden;
-          }
-          body {
-            background-color: rgb(168, 245, 206);
-            background-image: linear-gradient(-15deg, rgb(248, 198, 178), rgb(253, 237, 231));
-          }
-        `}</style>
       </Head>
       <body>
         <Main />
