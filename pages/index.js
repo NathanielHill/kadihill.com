539fabae79b3d3bddcddbbc2de9b79d7655dd151
Nathaniel Hill <nata@goguna.com>
2019-10-15 00:59:41 -0500

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,11 @@ export default () => (
       <title>Kadi Hill | Home</title>
     </Head>
     <h1>Welcome to KadiHill.com</h1>
-    <h2>Currently growing a baby and this website!</h2>
-    <h2>More content (and a baby) coming soon.</h2>
+    <h2>
+      Welcome! I want to share everything I've learned about pregnancy, birth and parenting that led me to two ecsatic
+      homebirths and a welath of knowledge from the Guna indians whom we have lived with for over two years.
+    </h2>
+    <h2>More content coming soon.</h2>
     <style jsx>{`
       h1 {
         text-align: center;
